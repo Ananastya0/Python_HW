9d8e126b1985607891893313bf6d0531d1fb2302
Ananastya0 <a.kovalyova98@gmail.com>
2024-10-26 12:13:21 +0300

    lesson_02

--- lesson_02/lesson_2_task_2.py
+++ lesson_02/lesson_2_task_2.py
@@ -1,6 +1,8 @@
-def is_yesr_leap(year):
-    if (year % 4 == 0):
-        print("год: " + str(year) + " True")
-    else:
-        print("год: " + str(year) + " False")
-is_yesr_leap(int(input("Введите год: ")))
+def is_year_leap(year):
+    if year % 4 == 0:
+        return True
+    return False
+
+year = int(input("Введите год: "))
+result = is_year_leap(year)
+print(f"год {year}: {result}")
